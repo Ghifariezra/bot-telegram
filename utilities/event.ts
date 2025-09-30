5a85c60f24f23037a99506c34e2a064488ef69be
Owner <ghifariezraramadhan@gmail.com>
2025-10-01 06:41:55 +0700

    UPDATE : debug

--- utilities/event.ts
+++ utilities/event.ts
@@ -184,10 +184,11 @@ Setelah kamu memilih, bot akan menampilkan *informasi cuaca terbaru* untuk wilay
         const weather = await this.getWeather(village);
         const dt = weather.data[0].cuaca.flat();
 
-        console.log(dt);
-
         const weatherNow = currentWeather(dt);
         const check = weatherNow;
+
+        console.log(dt);
+        console.log(check);
 
         if (!check) {
             await bot.sendMessage(chatId, `⚠️ Kelurahan "${village}" tidak ditemukan.`);
